e9c4943253bb55b4e441af7d5416a6eabbab975b
Shadowmaple <1142319190@qq.com>
2020-11-13 09:28:41 +0800

    update routers and original middlewares

--- router/router.go
+++ router/router.go
@@ -3,18 +3,54 @@ package router
 import (
 	"net/http"
 
+	"todolist_backend/handler/matter"
+	"todolist_backend/handler/sd"
+	"todolist_backend/handler/user"
+	"todolist_backend/router/middleware"
+
 	"github.com/gin-gonic/gin"
 )
 
+// Load loads the middlewares, routes, handlers.
 func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// Middlewares.
 	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	// 404 Handler.
 	g.NoRoute(func(c *gin.Context) {
-		c.String(http.StatusNotFound, "Incorrect API route")
+		c.String(http.StatusNotFound, "The incorrect API route.")
 	})
 
-	router := g.Group("api/v1")
+	// api for authentication functionalities
+	g.POST("/login", user.Login)
+
+	u := g.Group("/api/v1/user")
 	{
-		router.GET("", func(c *gin.Context) {})
+		u.POST("/login", user.Login)
+		u.GET("", user.Get)
+		u.PUT("", user.Update)
+	}
+
+	matterRouter := g.Group("api/v1/matter")
+	{
+		matterRouter.POST("", matter.Create)
+		matterRouter.GET("", matter.List)
+		matterRouter.GET("/:id", matter.Get)
+		matterRouter.PUT("/:id", matter.Update)
+		matterRouter.DELETE("/:id", matter.Delete)
+		matterRouter.PUT("/:id/finish", matter.Finish)
+	}
+
+	// The health check handlers
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
 	}
 
 	return g
